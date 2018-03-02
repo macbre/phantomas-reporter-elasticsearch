d1d4ff6644394d2ae56f245910c688798fcc2e4b
AdriwanKenoby <adri17ve@gmail.com>
2018-03-02 09:10:24 +0000

    support for ES6 API

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ module.exports = function(results, reporterOptions, options) {
 		render: function(done) {
 			var elasticsearch = require('elasticsearch'),
 				client = new elasticsearch.Client({
-					host: params.host
+					host: params.host,
+					log: 'trace'
 				}),
 				metrics = results.getMetricsNames(),
 				documentBody = {
@@ -49,8 +50,8 @@ module.exports = function(results, reporterOptions, options) {
 				},
 				mappingFields = {
 					url: {
-						type: 'string',
-						index: 'not_analyzed'
+						type: 'text',
+						index: false
 					},
 					reportDate: {
 						type: 'date'
@@ -58,10 +59,9 @@ module.exports = function(results, reporterOptions, options) {
 				};
 			// create and index an elasticsearch document with metrics data
 			function indexReport(documentBody) {
-				client.create({
+				client.index({
 					index: params.index,
-					type: params.type,
-					id: '',
+					type: params.type,					
 					body: documentBody
 				}, function(error, data) {
 					if (typeof error != "undefined") {
