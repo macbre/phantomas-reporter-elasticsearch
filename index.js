b5944c8eaf4b5501a6764db3211ff78246d1b7d0
AdriwanKenoby <adri17ve@gmail.com>
2018-03-05 21:16:37 +0000

    remove metrics ip to phantomas

--- index.js
+++ index.js
@@ -39,94 +39,87 @@ module.exports = function (results, reporterOptions, options) {
     return {
         render: function (done) {
             var elasticsearch = require('elasticsearch'),
-                publicIp = require('public-ip'),
                 client = new elasticsearch.Client({
                     host: params.host
                 }),
-                metrics = results.getMetricsNames();
-
-            publicIp.v4().then(function (ip) {
-                var documentBody = {
-                        url: results.getUrl(),
-                        reportDate: new Date(),
-                        reportFrom: ip
+                metrics = results.getMetricsNames(),
+                documentBody = {
+                    url: results.getUrl(),
+                    reportDate: new Date()
+                },
+                mappingFields = {
+                    url: {
+                        type: 'keyword',
+                        index: true
                     },
-                    mappingFields = {
-                        url: {
-                            type: 'keyword',
-                            index: true
-                        },
-                        reportDate: {
-                            type: 'date'
-                        },
-                        reportFrom: {
-                            type: 'ip'
-                        }
-                    };
-
-                // create and index an elasticsearch document with metrics data
-                function indexReport(documentBody) {
-                    client.index({
-                        index: params.index,
-                        type: params.type,
-                        body: documentBody
-                    }, function (error, data) {
-                        if (typeof error != "undefined") {
-                            done(new Error('Indexing error: ' + error));
-                        } else {
-                            debug('Stored under id %s', data._id);
-                        }
-                        done(undefined, {id: data._id});
-                    });
-                }
-
-                // store metrics value and mapping types
-                metrics.forEach(function (metric) {
-                    var value = results.getMetric(metric);
-                    documentBody[metric] = value;
-
-                    mappingFields[metric] = {
-                        type: (isNaN(value) ? 'text' : 'integer')
-                    };
-                });
-
-                client.indices.exists({
-                    index: params.index
-                }, function (err, exists) {
-                    if (typeof(err) == "undefined") {
-                        // index does not exists, we have to create it and define the mapping
-                        if (!exists) {
-                            client.indices.create({
-                                index: params.index
-                            }, function (err) {
-                                if (typeof(err) == "undefined") {
-                                    var mapping = {};
-                                    mapping[params.type] = {
-                                        properties: mappingFields
-                                    };
-                                    client.indices.putMapping({
-                                        type: params.type,
-                                        index: params.index,
-                                        body: mapping
-                                    }, function (err, data) {
-                                        if (typeof err == "undefined") {
-                                            indexReport(documentBody);
-                                        } else {
-                                            done(new Error('Create mapping error: ' + err));
-                                        }
-                                    });
-                                } else {
-                                    done(new Error('Create index error: ' + err));
-                                }
-                            });
-                        } else {
-                            indexReport(documentBody);
-                        }
-                    } else {
-                        done(new Error('Index exists check error:' + err));
+                    reportDate: {
+                        type: 'date'
                     }
+                };
+
+            // create and index an elasticsearch document with metrics data
+            function indexReport(documentBody) {
+                client.index({
+                    index: params.index,
+                    type: params.type,
+                    body: documentBody
+                }, function (error, data) {
+                    if (typeof error != "undefined") {
+                        done(new Error('Indexing error: ' + error));
+                    } else {
+                        debug('Stored under id %s', data._id);
+                    }
+                    done(undefined, {id: data._id});
                 });
+            }
+
+            // store metrics value and mapping types
+            metrics.forEach(function (metric) {
+                var value = results.getMetric(metric);
+                documentBody[metric] = value;
+
+                mappingFields[metric] = {
+                    type: (isNaN(value) ? 'text' : 'integer')
+                };
             });
+
+            client.indices.exists({
+                index: params.index
+            }, function (err, exists) {
+                if (typeof(err) == "undefined") {
+                    // index does not exists, we have to create it and define the mapping
+                    if (!exists) {
+                        client.indices.create({
+                            index: params.index
+                        }, function (err) {
+                            if (typeof(err) == "undefined") {
+                                var mapping = {};
+                                mapping[params.type] = {
+                                    properties: mappingFields
+                                };
+                                client.indices.putMapping({
+                                    type: params.type,
+                                    index: params.index,
+                                    body: mapping
+                                }, function (err, data) {
+                                    if (typeof err == "undefined") {
+                                        indexReport(documentBody);
+                                    } else {
+                                        done(new Error('Create mapping error: ' + err));
+                                    }
+                                });
+                            } else {
+                                done(new Error('Create index error: ' + err));
+                            }
+                        });
+                    } else {
+                        indexReport(documentBody);
+                    }
+                } else {
+                    done(new Error('Index exists check error:' + err));
+                }
+            });
+
         }
     };
 };
